01fe56b27d69cf9323326d281844a34031bd18c7
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2025-12-10 09:49:24 +0100

    re-factor table rendering

--- internal/ui/components/device_table.go
+++ internal/ui/components/device_table.go
@@ -50,8 +50,6 @@ func NewDeviceTable(d tableDelegate) *widget.Table {
 
 func updateCell(d tableDelegate, id widget.TableCellID, obj fyne.CanvasObject) {
 	cell := obj.(*fyne.Container)
-	btn := cell.Objects[0].(*widget.Button)
-	lbl := cell.Objects[1].(*widget.Label)
 
 	items := d.GetDeviceModel().FilteredData.Items
 	if id.Row >= len(items) {
@@ -60,30 +58,84 @@ func updateCell(d tableDelegate, id widget.TableCellID, obj fyne.CanvasObject) {
 	item := items[id.Row]
 
 	// Data Columns
-	if id.Col < len(model.DeviceColumns)-1 {
-		btn.Hide()
-		lbl.Show()
+	if id.Col < len(model.DeviceColumns) {
 		switch id.Col {
 		case 0:
-			lbl.SetText(item.Title)
+			updateTitleCell(d, cell, item)
 		case 1:
-			lbl.SetText(item.Status)
+			updateStatusCell(d, cell, item)
 		case 2:
-			if len(item.AncestorsTitles) > 0 {
-				lbl.SetText(strings.Join(item.AncestorsTitles[:len(item.AncestorsTitles)-1], " > "))
-			} else {
-				lbl.SetText("")
-			}
+			updateGroupCell(d, cell, item)
 		case 3:
-			lbl.SetText(strings.Join(item.Tags, ", "))
+			updateTagsCell(d, cell, item)
 		case 4: // Connection Info Column
-			lbl.SetText(updateConnectionStatus(d, item))
+			updateConnectionStatusCell(d, cell, item)
+		case 5:
+			updateActionCell(d, cell, item)
 		}
-		return
 	}
+}
 
-	// Action Column
-	lbl.Hide()
+func updateTitleCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	title := widget.NewLabel(item.Title)
+	title.Truncation = fyne.TextTruncateEllipsis
+	cell.Add(title)
+}
+
+func updateStatusCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	var statusIcon fyne.Resource
+	var colorName fyne.ThemeColorName
+
+	if item.Status == "online" {
+		statusIcon = theme.ConfirmIcon()
+		colorName = theme.ColorNameSuccess
+	} else {
+		statusIcon = theme.CancelIcon()
+		colorName = theme.ColorNameError
+	}
+	themeResource := theme.NewThemedResource(statusIcon)
+	themeResource.ColorName = colorName
+	status := widget.NewIcon(themeResource)
+
+	cell.Add(status)
+}
+
+func updateTagsCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	tags := "-"
+
+	if len(item.Tags) > 0 {
+		tags = strings.Join(item.Tags, ", ")
+	}
+	tagLabel := widget.NewLabel(tags)
+	tagLabel.Truncation = fyne.TextTruncateEllipsis
+	cell.Add(tagLabel)
+}
+
+func updateGroupCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	text := ""
+	if len(item.AncestorsTitles) > 0 {
+		text = strings.Join(item.AncestorsTitles[:len(item.AncestorsTitles)-1], " > ")
+	}
+	group := widget.NewLabel(text)
+	group.Truncation = fyne.TextTruncateEllipsis
+	cell.Add(group)
+}
+
+func updateConnectionStatusCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	text := updateConnectionStatus(d, item)
+	status := widget.NewLabel(text)
+	status.Truncation = fyne.TextTruncateEllipsis
+	cell.Add(status)
+}
+
+func updateActionCell(d tableDelegate, cell *fyne.Container, item client.InventoryListItem) {
+	cell.RemoveAll()
+	btn := widget.NewButton("", nil)
 	conn, ok := d.GetStore().GetActive(item.NodeID)
 	if ok {
 		btn.SetIcon(theme.CancelIcon())
@@ -96,10 +148,14 @@ func updateCell(d tableDelegate, id widget.TableCellID, obj fyne.CanvasObject) {
 		btn.SetIcon(theme.SettingsIcon())
 		btn.OnTapped = func() { d.ShowConnectDialog(&item) }
 	}
-	btn.Show()
+
+	cell.Add(btn)
 }
 
 func updateHeader(d tableDelegate, id widget.TableCellID, obj fyne.CanvasObject) {
+	if id.Col < 0 {
+		return
+	}
 	if id.Col >= len(model.DeviceColumns) {
 		return
 	}
@@ -153,7 +209,7 @@ func updateConnectionStatus(d tableDelegate, item client.InventoryListItem) stri
 	for _, target := range activeConn.Targets {
 		targetStrings = append(
 			targetStrings,
-			fmt.Sprintf("%s: %s:%s -> %s:%s", target.Protocol, target.LocalHost, target.LocalPort, target.RemoteHost, target.RemotePort),
+			fmt.Sprintf("%s: %s:%s > %s:%s", target.Protocol, target.LocalHost, target.LocalPort, target.RemoteHost, target.RemotePort),
 		)
 	}
 
